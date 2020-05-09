a20347d7dec57a2db74c88699f44f73284679170
John Doe <viktorsworkmail@gmail.com>
2020-05-09 13:36:18 -0700

    updated location cache

--- src/main/java/com/inf191/lachesis/Controller/LocationdataController.java
+++ src/main/java/com/inf191/lachesis/Controller/LocationdataController.java
@@ -1,6 +1,7 @@
 package com.inf191.lachesis.Controller;
 
 
+import com.inf191.lachesis.Helper.RecentLocationCache;
 import com.inf191.lachesis.Server.LocationdataServiceImpl;
 import com.inf191.lachesis.generate.Fitbitdata;
 import com.inf191.lachesis.generate.Locationdata;
@@ -14,11 +15,14 @@ import javax.validation.Valid;
 @CrossOrigin
 public class LocationdataController {
 
+    //set up the cache with size 10
+    RecentLocationCache<String,Locationdata> LRU = new RecentLocationCache<>(10);
+
     @Autowired
     LocationdataServiceImpl locationdataServiceImpl;
 
     @RequestMapping(method= RequestMethod.GET ,value ="/getbylid={lid}")
-    private Locationdata getLocation(@PathVariable("lid") int lid){
+    public Locationdata getLocation(@PathVariable("lid") int lid){
         return locationdataServiceImpl.selectByPrimaryKey(lid);
     }
 
@@ -35,13 +39,24 @@ public class LocationdataController {
 
     @PostMapping( "/insertLocationByJSON")
     public int insertLocation(@RequestBody @Valid Locationdata locationdata){
+
+
+
         Locationdata ldata = new Locationdata();
+
         ldata.setMid(locationdata.getMid());
-            ldata.setLocation(locationdata.getLocation());
+        if(locationdata.getLocation().equalsIgnoreCase("adt35s6d7b78dh9k06d9ns126s8bd7fj3")){
+            ldata.setLocation("Room A");
 
-
+        }
         ldata.setTime(locationdata.getTime());
-
+        LRU.put(ldata.getMid(),ldata);
         return locationdataServiceImpl.insert(ldata);
     }
-}
+
+    @RequestMapping(method= RequestMethod.GET ,value ="/getRecentbymid={mid}")
+    public Locationdata getRecentLocation(@PathVariable("mid") String mid) {
+        return LRU.get(mid);
+    }
+
+    }
---
+++ src/main/java/com/inf191/lachesis/Helper/RecentLocationCache.java
@@ -0,0 +1,89 @@
+package com.inf191.lachesis.Helper;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RecentLocationCache<K, V> {
+    private static final float hashTableLoadFactor = 0.75f;
+    private LinkedHashMap<K, V> map;
+    private int cacheSize;
+
+    /**
+     * Creates a new LRU cache. 在该方法中，new LinkedHashMap<K,V>(hashTableCapacity,
+     * hashTableLoadFactor, true)中，true代表使用访问顺序
+     *
+     * @param cacheSize
+     *            the maximum number of entries that will be kept in this cache.
+     */
+    public RecentLocationCache(int cacheSize) {
+        this.cacheSize = cacheSize;
+        int hashTableCapacity = (int) Math
+                .ceil(cacheSize / hashTableLoadFactor) + 1;
+        map = new LinkedHashMap<K, V>(hashTableCapacity, hashTableLoadFactor,
+                true) {
+            // (an anonymous inner class)
+            private static final long serialVersionUID = 1;
+
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return size() > RecentLocationCache.this.cacheSize;
+            }
+        };
+    }
+
+    /**
+     * Retrieves an entry from the cache.<br>
+     * The retrieved entry becomes the MRU (most recently used) entry.
+     *
+     * @param key
+     *            the key whose associated value is to be returned.
+     * @return the value associated to this key, or null if no value with this
+     *         key exists in the cache.
+     */
+    public synchronized V get(K key) {
+        return map.get(key);
+    }
+
+    /**
+     * Adds an entry to this cache. The new entry becomes the MRU (most recently
+     * used) entry. If an entry with the specified key already exists in the
+     * cache, it is replaced by the new entry. If the cache is full, the LRU
+     * (least recently used) entry is removed from the cache.
+     *
+     * @param key
+     *            the key with which the specified value is to be associated.
+     * @param value
+     *            a value to be associated with the specified key.
+     */
+    public synchronized void put(K key, V value) {
+        map.put(key, value);
+    }
+
+    /**
+     * Clears the cache.
+     */
+    public synchronized void clear() {
+        map.clear();
+    }
+
+    /**
+     * Returns the number of used entries in the cache.
+     *
+     * @return the number of entries currently in the cache.
+     */
+    public synchronized int usedEntries() {
+        return map.size();
+    }
+
+    /**
+     * Returns a <code>Collection</code> that contains a copy of all cache
+     * entries.
+     *
+     * @return a <code>Collection</code> with a copy of the cache content.
+     */
+    public synchronized Collection<Map.Entry<K, V>> getAll() {
+        return new ArrayList<Map.Entry<K, V>>(map.entrySet());
+    }
+}
